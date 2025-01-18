c811bc9e5885deb583d1bb0234236251627b46fd
djzib0 <piotrsabatura@gmail.com>
2025-01-18 08:00:56 +0100

    WIP: trying to solve form problem

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,68 +1,157 @@
-"use client";
+'use client'
 
-import React, { useState, useActionState, FormEvent } from "react";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import MDEditor from "@uiw/react-md-editor";
-import { Button } from "@/components/ui/button";
-import { Send } from "lucide-react";
-import { formSchema } from "@/lib/validation";
-import { z } from "zod";
-import { useToast } from "@/hooks/use-toast";
-import { useRouter } from "next/navigation";
-import { createPitch } from "@/lib/actions";
+import React, { useActionState, useState } from 'react';
+import { Input } from './ui/input';
+import MDEditor from '@uiw/react-md-editor'
+import { Button } from './ui/button';
+import { Send } from 'lucide-react';
+import { formSchema } from '@/lib/validation';
 
 const StartupForm = () => {
-  const [errors, setErrors] = useState<Record<string, string>>({});
-  const router = useRouter();
 
+  const [errors, setErrors] = useState<Record<string, string>>({});
+  // markdown editor state
+  const [pitch, setPitch] = useState("")
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleFormSubmit = async (prevState: any, formData: FormData) => {
-    console.log("I'm here")
     try {
       const formValues = {
         title: formData.get("title") as string,
-      };
+        description: formData.get("description") as string,
+        category: formData.get("category") as string,
+        link: formData.get("link") as string,
+        pitch,
+      }
 
       await formSchema.parseAsync(formValues);
 
-      console.log(formValues)
+      // const result = await createDiffieHellman(prevState, formData, pitch);
 
-      // const result = await createPitch(prevState, formData, pitch);
-      return prevState
+      console.log("I'm here")
+
+      return {
+        ...prevState,
+        ...formValues
+      }
     } catch (error) {
-      console.log(error)
-    }
-  };
+      return error
+    } finally {
 
-  const [state, formAction] = useActionState(handleFormSubmit, null);
+    }
+
+    return {
+      ...prevState,
+      title: formData.get("title"),
+      description: formData.get("description"),
+      category: formData.get("category"),
+      link: formData.get("link")
+    }
+  }
+
+  const [state, formAction, isPending] = useActionState(
+    handleFormSubmit,
+    {
+      error: "",
+      status: "INITIAL"
+    },
+  )
+
+  console.log(state, " state")
 
   return (
-    <form action={formAction} className="startup-form">
-      <div>
-        <label htmlFor="title" className="startup-form_label">
-          Title
-        </label>
-        <Input
-          id="title"
-          name="title"
-          className="startup-form_input"
-          required
-          placeholder="Startup Title"
-        />
+    <div>
+      <form action={formAction} className='startup-form'>
 
-        {errors.title && <p className="startup-form_error">{errors.title}klj</p>}
-      </div>
+        <div>
+          <label htmlFor='title' className='startup-form_label'>
+            Title
+          </label>
+          <input 
+            id='title'
+            name='title'
+            className='startup-form_input'
+            required
+            placeholder='Startup Title'
+          />
+          {errors.title && <p className='startup-form_error'>{errors.title}</p>}
+        </div>
 
-      <Button
-        type="button"
-        className="startup-form_btn text-white"
-        // disabled={isPending}
-      >
-        {/* {isPending ? "Submitting..." : "Submit Your Pitch"} */}
-        <Send className="size-6 ml-2" />
-      </Button>
-    </form>
-  );
-};
+        <div>
+          <label htmlFor='description' className='startup-form_label'>
+            Description
+          </label>
+          <input 
+            id='description'
+            name='description'
+            className='startup-form_input'
+            required
+            placeholder='Startup Description'
+          />
+          {errors.description && <p className='startup-form_error'>{errors.description}</p>}
+        </div>
 
-export default StartupForm;
+        <div>
+          <label htmlFor='category' className='startup-form_label'>
+            Category
+          </label>
+          <input 
+            id='category'
+            name='category'
+            className='startup-form_input'
+            required
+            placeholder='Startup Category (Tech, Healthm, Education ...)'
+          />
+          {errors.category && <p className='startup-form_error'>{errors.category}</p>}
+        </div>
+
+        <div>
+          <label htmlFor='link' className='startup-form_label'>
+            Image URL
+          </label>
+          <input 
+            id='link'
+            name='link'
+            className='startup-form_input'
+            required
+            placeholder='Startup Image URL'
+          />
+          {errors.link && <p className='startup-form_error'>{errors.link}</p>}
+        </div>
+
+        <div data-color-mode="light">
+          <MDEditor
+            value={pitch}
+            onChange={(value) => setPitch(value as string)}
+            id='pitch'
+            preview='edit'
+            height={300}
+            style={{borderRadius: 20, overflow: "hidden"}}
+            textareaProps={{
+              placeholder: "Briefly describe your idea and what problem it solves",
+            }}
+            previewOptions={{
+              disallowedElements: ["style"]
+            }}
+           />
+          {errors.pitch && <p className='startup-form_error'>{errors.pitch}</p>}
+        </div>
+        <Button 
+          type='submit' 
+          className='startup-form_btn text-white'
+          disabled={isPending}
+          >{isPending ? 'Submitting...' : 'Submit Your Pitch'}
+          <Send className='size-6 ml-2' />
+        
+        </Button>
+        
+      </form>
+      <p>Title: {state?.title}</p>
+      <p>Description: {state?.description}</p>
+      <p>Category: {state?.category}</p>
+      <p>Image URL: {state?.link}</p>
+    </div>
+  )
+}
+
+export default StartupForm
--- components/TestForm.tsx
+++ components/TestForm.tsx
@@ -1,21 +1,13 @@
 'use client'
 
-import React, { useActionState } from 'react'
+import React from 'react'
 
 const TestForm = () => {
 
-  async function handleForm(prevState, formData) {
-    console.log(prevState)
-  }
-
-  const [state, formAction] = useActionState(handleForm, 25)
-
+  
   return (
     <div>
-      <form>
-        {state}
-        <button formAction={formAction}>Do something</button>
-      </form>
+      Test Form
     </div>
   )
 }
